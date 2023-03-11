5c46effe8f333cd332e5dcaabe2fdc23fdc29c3e
Hoàng Minh Quân <81216869+minhquan490@users.noreply.github.com>
2023-03-11 17:17:20 +0700

    Remove hard code when read properties file

--- server/src/main/java/com/order/bachlinh/server/loader/internal/DefaultServerPropertiesLoader.java
+++ server/src/main/java/com/order/bachlinh/server/loader/internal/DefaultServerPropertiesLoader.java
@@ -3,18 +3,10 @@ package com.order.bachlinh.server.loader.internal;
 import com.order.bachlinh.server.loader.spi.ServerPropertiesLoader;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 class DefaultServerPropertiesLoader implements ServerPropertiesLoader {
-    private static final String DELIMITER = "/";
-    private final String resourcePath;
-
-    DefaultServerPropertiesLoader() {
-        String root = new File("").getAbsolutePath();
-        this.resourcePath = String.join(DELIMITER, root, "server", "src", "main", "resources");
-    }
 
     @Override
     public void loadDefault() {
@@ -36,7 +28,7 @@ class DefaultServerPropertiesLoader implements ServerPropertiesLoader {
             while ((line = bufferedReader.readLine()) != null) {
                 String[] keyPairValue = line.split("=");
                 if (keyPairValue[1].startsWith("classpath:")) {
-                    System.setProperty(keyPairValue[0], String.join(DELIMITER, resourcePath, keyPairValue[1].split("classpath:")[1]));
+                    System.setProperty(keyPairValue[0], classLoader.getResource(keyPairValue[1].split("classpath:")[1]).getPath());
                 } else {
                     System.setProperty(keyPairValue[0], keyPairValue[1]);
                 }
